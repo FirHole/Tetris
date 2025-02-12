aa7675609d8e9b5c8199b06f98c3ea369736abd8
FirHole <okairoblox@gmail.com>
2025-02-12 22:33:26 +0300

    Finally 1.0

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import randint
 import time, sys
 import figures_loader
 
-size = 30
+size = 50
 
 
 def new_data():
@@ -16,15 +16,15 @@ def new_data():
             break
 
     return {'figure': figure[0],
-            'x': (field_size // xm - len(figure[0][0])) // 2,
-            'y': ((field_size - len(figure[0])) // 2 if xm == 1 else 0),
+            'x': (field_size // 2 - len(figure[0][0])) // 2,
+            'y': 0,
             'color': figure[1],
             'next': next_figure
             }
 
 
 def message(text):
-    p.draw.rect(SCREEN, (100, 100, 100), p.Rect(0, 8.5 * size, (20 if gravity else 10) *  size, size), 0)
+    p.draw.rect(SCREEN, (70, 75, 150), p.Rect(0, 8.5 * size, 10.1 *  size, size), 0)
     SCREEN.blit( p.font.SysFont('comicsans', int(size/2)+3, True).render(text, True, (255, 255, 255)), (0, 8.55 * size))
     p.display.flip()
 
@@ -56,7 +56,7 @@ def exit_button(event = None):
 
 
 def custom():
-    modes = [p.Rect(size * 1, size * 2, size * 8, size * 5),  p.Rect(size * 11, size * 2, size * 8, size * 5), p.Rect(size * 1, size * 8, size * 8, size * 5),  p.Rect(size * 11, size * 8, size * 8, size * 5)]
+    modes = [p.Rect(size * 1, size * 2, size * 8, size * 5),  p.Rect(size * 11, size * 2, size * 8, size * 5), p.Rect(size * 6, size * 8, size * 8, size * 5)]
     selected_mode = modes[0]
     pieces = [p.Rect(size * 3.125, size * 14, size * 3.75, size * 3), p.Rect(size * 8.125, size * 14, size * 3.75, size * 3),  p.Rect(size * 13.125, size * 14, size * 3.75, size * 3)]
     selected_pieces = [pieces[1]]
@@ -71,7 +71,7 @@ def custom():
         else:
             SCREEN.blit(p.transform.scale(p.image.load('assets/play_custom.png'), (play[2], play[3])), play)
 
-        for button, asset in zip(modes + pieces + [play], ['default_mode', 'linear_mode', 'dripping_mode', 'random_mode', '123minoes', '4minoes', '5minoes']):
+        for button, asset in zip(modes + pieces + [play], ['default_mode', 'linear_mode', 'dripping_mode', '123minoes', '4minoes', '5minoes']):
                 if selected_mode == button or button in selected_pieces:
                     SCREEN.blit(p.transform.scale(p.image.load('assets/'+asset+'_selected.jpg'), (button[2], button[3])), button)
                 else:
@@ -135,16 +135,16 @@ def update():
     SCREEN.fill((0, 0, 0))
     global hold, level, points, lines_counter
 
-    for x in range(0, size * 20 // xm, block):
+    p.draw.rect(SCREEN, ((70, 75, 150) if move_time > 0 else (80, 85, 160)), p.Rect(size * 10, 0, size * 20, size * 20), 0)
+
+    for x in range(0, size * (10 if field_size == 30 else 11), block):
         p.draw.line(SCREEN, (50, 50, 50), (x, 0), (x, size * 20), 2)
 
-    p.draw.rect(SCREEN, (100, 100, 100), p.Rect(size * (20 if gravity else 10), 0, size * 20, size * 20), 0)
 
+    SCREEN.blit( p.font.SysFont('comicsans', size, True).render('NEXT:', True, (255, 255, 255)), (size * 13.3, size * 0.5))
+    SCREEN.blit( p.font.SysFont('comicsans', size, True).render('HOLD:', True, (255, 255, 255)), (size * 13.3, size * 8))
 
-    SCREEN.blit( p.font.SysFont('comicsans', size, True).render('NEXT:', True, (255, 255, 255)), (size * (23.3 if gravity else 13.3), size * 0.5))
-    SCREEN.blit( p.font.SysFont('comicsans', size, True).render('HOLD:', True, (255, 255, 255)), (size *(23.3 if gravity else 13.3), size * 8))
-
-    x = (22.5 if gravity else 12.5)
+    x = 12.5
     for y in [size * 2, size * 9.5]:
         SCREEN.blit(p.transform.scale(bg, (size * 5, size * 5)), (size * x, y))
         p.draw.lines(SCREEN, (50, 50, 50), True, [(size * x, y), (size * (x + 5), y), (size * (x + 5), y + 5 * size), (size * x, y + 5 * size)], 5)
@@ -152,7 +152,7 @@ def update():
     for y, row in enumerate(data['next'][0]):
         for x, obj in enumerate(row):
             if obj:
-                posx, posy = ((25 if gravity else 15) - len(row)/2 + x) * size, (4.5 - len(data['next'][0]) / 2 + y) * size
+                posx, posy = (15 - len(row)/2 + x) * size, (4.5 - len(data['next'][0]) / 2 + y) * size
                 piece = p.Rect(posx, posy, size, size)
                 p.draw.rect(SCREEN, data['next'][1], piece, 0)
                 p.draw.lines(SCREEN, (210, 210, 210), True, [(posx, posy), (posx + size, posy), (posx + size, posy + size),  (posx, posy + size)], 1)
@@ -160,16 +160,16 @@ def update():
     for y, row in enumerate(hold['figure']):
         for x, obj in enumerate(row):
             if obj:
-                posx, posy = ((25 if gravity else 15) - len(row)/2 + x) * size, (12 - len(hold['figure']) / 2 + y) * size
+                posx, posy = (15 - len(row)/2 + x) * size, (12 - len(hold['figure']) / 2 + y) * size
                 piece = p.Rect(posx, posy, size, size)
                 p.draw.rect(SCREEN, hold['color'], piece, 0)
                 p.draw.lines(SCREEN, (210, 210, 210), True, [(posx, posy), (posx + size, posy), (posx + size, posy + size),  (posx, posy + size)], 1)
 
 
-    SCREEN.blit( p.font.SysFont('comicsans', size, True).render('LEVEL ' + str(level), True, (255, 255, 255)), (size * (23 if gravity else 13), size * 16))
-    SCREEN.blit( p.font.SysFont('comicsans', size//2, True).render('Next level in: ' + str(10 - lines_counter) + ' lines', True, (255, 255, 255)), (size * (22.5 if gravity else 12.5), size * 17.2))
+    SCREEN.blit( p.font.SysFont('comicsans', size, True).render('LEVEL ' + str(level), True, (255, 255, 255)), (size * 13, size * 16))
+    SCREEN.blit( p.font.SysFont('comicsans', size//2, True).render('Next level in: ' + str(10 - lines_counter) + ' lines', True, (255, 255, 255)), (size * 12.5, size * 17.2))
 
-    SCREEN.blit( p.font.SysFont('comicsans', size, True).render('SCORE: ' + str(points), True, (255, 255, 255)), (size * (22 if gravity else 12), size * 18.5))
+    SCREEN.blit( p.font.SysFont('comicsans', size, True).render('SCORE: ' + str(points), True, (255, 255, 255)), (size * 12, size * 18.5))
 
     for y, row in enumerate(field):
         for x, obj in enumerate(row):
@@ -207,7 +207,7 @@ def update():
 
     for y, row in enumerate(data['figure']):
         for x, obj in enumerate(row):
-            if obj:
+            if obj and ypos - 1 != data['y']:
                 piece = p.Rect((x + data['x']) * block, (y + ypos - 1) * block, block, block)
                 p.draw.rect(SCREEN, (40, 40, 40), piece, 0)
                 p.draw.lines(SCREEN, (70, 70, 70), True, [((x + data['x']) * block, (y + ypos - 1) * block), ((x + data['x']) * block + block, (y + ypos - 1) * block), ((x + data['x']) * block + block, (y + ypos - 1) * block + block), ((x + data['x']) * block, (y + ypos - 1) * block + block)], 1)
@@ -216,13 +216,12 @@ def update():
 
 
 def game(settings = [0, [1]]):
-    global hold, level, points, lines_counter, data, field, figures, keys, next_figure, block, field_size, bg, gravity, xm
+    global hold, level, points, lines_counter, data, field, figures, keys, next_figure, block, field_size, bg, move_time
 
     figures, multiplier = figures_loader.main(settings[1], settings[0])
     block = int(size / multiplier)
     field_size = int(20 * multiplier)
-    xm = 2
-    gravity = 0
+
     keys = list(figures.keys())
     next_figure = [figures[keys[randint(0, len(keys) - 1)]], (randint(50, 200), randint(50, 200), randint(50, 200))]
 
@@ -236,11 +235,9 @@ def game(settings = [0, [1]]):
     bg = p.image.load('assets/custom_bg.png')
 
     if settings[0] == 3:
-        gravity = randint(1, 4)
-        xm = 1
-        p.display.set_mode((size * 30, size * 20))
+        pass
 
-    field = [[0 for _ in range(field_size // xm)] for _ in range(field_size)]
+    field = [[0 for _ in range(field_size // 2)] for _ in range(field_size)]
     data = new_data()
 
     while True:
@@ -253,7 +250,7 @@ def game(settings = [0, [1]]):
                 level = 1
                 lines_counter = 0
                 points = 0
-                field = [[0 for _ in range(field_size // xm)] for _ in range(field_size)]
+                field = [[0 for _ in range(field_size // 2)] for _ in range(field_size)]
                 continue
 
             if settings[0] == 2:
@@ -275,13 +272,13 @@ def game(settings = [0, [1]]):
                 for obj in row:
                     if obj:
                         blocks += 1
-                if blocks == field_size // xm:
-                    field = [[0 for _ in range(field_size // xm)]] + field[0:y] + field[y+1:field_size]
+                if blocks == field_size // 2:
+                    field = [[0 for _ in range(field_size // 2)]] + field[0:y] + field[y+1:field_size]
                     lines += 1
 
             for y in range(0, len(field) - 1):
                 if field[y][0] == (30, 30, 40) and field[y+1][0] == (30, 30, 40):
-                    field = [[0 for _ in range(field_size // xm)]] + field[0:y] + field[y+1:field_size]
+                    field = [[0 for _ in range(field_size // 2)]] + field[0:y] + field[y+1:field_size]
 
 
             match lines:
@@ -329,7 +326,7 @@ def game(settings = [0, [1]]):
                     data['x'] += 1
                     for y, row in enumerate(data['figure']):
                         for x, obj in enumerate(row):
-                            if obj and (x + data['x'] >= field_size // xm or field[y + data['y']][x + data['x']] != 0):
+                            if obj and (x + data['x'] >= field_size // 2 or field[y + data['y']][x + data['x']] != 0):
                                 data['x'] -= 1
 
                 if event.key == p.K_UP:
@@ -374,9 +371,8 @@ def game(settings = [0, [1]]):
                                 continue
                             break
 
+SCREEN = p.display.set_mode((size * 20, size * 20))
 p.init()
 
 while True:
-    SCREEN = p.display.set_mode((size * 20, size * 20))
-    print('resized')
     menu()
